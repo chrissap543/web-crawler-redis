a91c242bcbbf6c887d6c71db858acded1639352a
chrissap543 <cs79@illinois.edu>
2025-06-23 15:55:30 -0700

    added templates for ecs deployment

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 	"github.com/go-redis/redis/v8"
 
 	"web-crawler/internal/myredis"
 	"web-crawler/internal/scraper"
 )
 
-func connectToRedis() (*redis.Client) {
+func connectToRedis(maxAttempts int) (*redis.Client) {
 	redisHost := os.Getenv("REDIS_HOST")
 	if redisHost == "" {
 		redisHost = "localhost"
@@ -21,25 +22,37 @@ func connectToRedis() (*redis.Client) {
 		redisPort = "6379"
 	}
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", redisHost, redisPort),
-		Password: "",
-		DB: 0,
-	});
-	ctxt := context.Background()
+	addr := fmt.Sprintf("%s:%s", redisHost, redisPort)
 
-	pong, err := rdb.Ping(ctxt).Result()
-	if err != nil {
-		log.Fatalf("Failed to ping the redis server")
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		log.Printf("Attempting to connect with attempt %d", attempt)
+		rdb := redis.NewClient(&redis.Options{
+			Addr: addr,
+			Password: "",
+			DB: 0,
+		});
+
+		ctxt, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+		result, err := rdb.Ping(ctxt).Result()
+		cancel()
+
+		if err == nil && result == "PONG" {
+			log.Printf("Connected to redis server with pong: %s", result)
+		}
+		log.Printf("Failed to ping the redis server")
+		rdb.Close()
+
+		log.Printf("Sleeping for 2 seconds")
+		time.Sleep(time.Duration(2) * time.Second)
 	}
-	fmt.Printf("Connected to redis server with pong: %s\n", pong)
-
-	return rdb
+	log.Fatalf("Failed to connect to Redis after %d attempts", maxAttempts)
+	return nil
 }
 
 func main() {
 	fmt.Println("Trying to connect to redis server")
-	rdb := connectToRedis()
+	rdb := connectToRedis(10)
+	defer rdb.Close()
 
 	fmt.Println("Building queue/set")
 	redisQueue := myredis.NewRedisQueue(rdb, "websites")
